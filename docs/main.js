540652a3c7aedcda61624ab4b5e0ba1c0bbab1af
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-12 21:20:12 +0800

    add: head

--- docs/main.js
+++ docs/main.js
@@ -9,8 +9,8 @@ const table1 = Listable("table", {
   ],
 });
 
-table1.addHead(["Fullname", "Age", "Address"]);
-table1.addRow(["Mart Salazar", 20, "Happy Homes Fairview"]);
-table1.addRow(["Anthony", 20, "Happy Homes Fairview"]);
-table1.addRow(["asdasd", 20, "Happy Homes Fairview"]);
-table1.addRow(["asfg", 20, "Happy Homes Fairview"]);
+table1.addHead(["Fullname", "Age", "Country"]);
+table1.addRow(["Mart Salazar", 20, "PH"]);
+table1.addRow(["Anthony", 20, "PH"]);
+table1.addRow(["asdasd", 20, "PH"]);
+table1.addRow(["asfg", 20, "PH"]);
