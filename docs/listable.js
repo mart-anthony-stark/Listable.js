43266c25dbdb77e53103d8d559e98899c4d7536b
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-12 12:54:40 +0800

    add: custom web component

--- docs/listable.js
+++ docs/listable.js
@@ -2,6 +2,11 @@ const Listable = () => {
   class ListableComponent extends HTMLElement {
     constructor() {
       super();
+      const className = this.getAttribute("class");
+      const id = this.getAttribute("id");
+      const theme = this.getAttribute("theme");
     }
   }
+
+  window.customElements.define("list-table", ListableComponent);
 };
