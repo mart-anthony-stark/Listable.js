31aec0f2366f07f92b5189b192117f16b69bb8ba
mart-anthony-stark <martsalazar0@gmail.com>
2022-02-12 21:15:56 +0800

    remove: styles

--- docs/listable.js
+++ docs/listable.js
@@ -1,45 +1,7 @@
 const Listable = (id, options = {}) => {
   const template = document.createElement("div");
   template.innerHTML = `
-        <style>
-        .listable {
-            display: flex;
-            flex-direction: column;
-            max-width: 400px;
-            justify-content: center;
-            align-items: center;
-            gap: 15px;
-            padding: 5px;
-            background: rgb(245, 245, 245);
-          }
-          .listable input#listable-search {
-            padding: 5px;
-            align-self: flex-start;
-          }
-          .listable table {
-            background: rgb(219, 219, 219);
-            width: 100%;
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-            border: 1px solid black;
-            border-collapse: collapse;
-          }
-          .listable table td,
-          .listable table th {
-            padding: 10px;
-            text-align: center;
-          }
-          .listable table thead {
-            border-bottom: 1px solid black;
-          }
-          .listable table tbody tr:hover {
-            cursor: pointer;
-            background: rgb(230, 230, 230);
-          }
-          </style>
-
-        <div class="listable" id=${id}>
+          <div class="listable" id=${id}>
         <input id="listable-search" type="text" placeholder="Search">
         <table>
             <thead>
